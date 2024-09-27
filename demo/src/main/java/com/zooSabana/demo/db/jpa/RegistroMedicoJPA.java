612cb87cc22d516eccf64e4a8c3c32b44503bf42
Ana3Lu <anaquiva@unisabana.edu.co>
2024-09-27 16:30:26 -0500

    Cambio traer id de animales en endpoint animales sin registro

--- demo/src/main/java/com/zooSabana/demo/db/jpa/RegistroMedicoJPA.java
+++ demo/src/main/java/com/zooSabana/demo/db/jpa/RegistroMedicoJPA.java
@@ -2,6 +2,8 @@ package com.zooSabana.demo.db.jpa;
 
 import com.zooSabana.demo.db.orm.RegistroMedicoORM;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,5 +12,6 @@ public interface RegistroMedicoJPA extends JpaRepository<RegistroMedicoORM, Long
 
     List<RegistroMedicoORM> findByAnimal_Id(Long animal_id);
 
-    List<Long> findDistinctAnimalIdsByFechaBetween(LocalDate inicio, LocalDate fin);
+    @Query("SELECT DISTINCT r.animal.id FROM RegistroMedicoORM r WHERE r.fecha BETWEEN :inicio AND :fin")
+    List<Long> findDistinctAnimalIdsByFechaBetween(@Param("inicio") LocalDate inicio, @Param("fin") LocalDate fin);
 }
