94b781e08fc8a818864890c107adc03dab0016f1
Ana3Lu <anaquiva@unisabana.edu.co>
2024-11-21 16:42:39 -0500

    Notificación automática

--- demo/src/main/java/com/zooSabana/demo/mensajeria/logica/PublicacionService.java
+++ demo/src/main/java/com/zooSabana/demo/mensajeria/logica/PublicacionService.java
@@ -1,6 +1,7 @@
 package com.zooSabana.demo.mensajeria.logica;
 
 import com.zooSabana.demo.mensajeria.productores.ProductorAuditoria;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,13 @@ public class PublicacionService {
     }
 
     public void manejarPublicacion() {
+        System.out.println("Ejecutando publicación (manual o automática)...");
         productorAuditoria.publicarAnimalesSinRevision();
+    }
+
+    @Scheduled(cron = "0 */4 * * * *") // Cada 4 minutos
+    public void manejarPublicacionAutomatica() {
+        System.out.println("Ejecutando tarea programada de publicación...");
+        manejarPublicacion();
     }
 }
